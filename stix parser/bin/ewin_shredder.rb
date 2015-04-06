43f395ebabf15092ceb878b5f79ace28382d2695
Tim <tim.pegg@gmail.com>
2015-04-06 13:29:21 -0500

    Better handling for missing arguments Added the abilty to handle manual entries Database culls entries older than a year.

--- stix parser/bin/ewin_shredder.rb
+++ stix parser/bin/ewin_shredder.rb
@@ -10,14 +10,17 @@ require 'ewinparser/scrubber'
 require 'ewinparser/spreadsheet_parser'
 require 'ewinparser/printer'
 
-
 def main
   opts = nil
 
+  # TODO show missing argument
   begin
     opts = Ewinparser::CliParser.parse(ARGV)
   rescue OptionParser::InvalidOption => e
     puts e.message, ""
+    help
+  rescue OptionParser::MissingArgument => e
+    puts e.message, "-o FILE and either -i FILENAME or -m FILENAME is required\n\n"
     help
   end
 
@@ -30,81 +33,109 @@ def main
   begin
 
     @database =  Ewinparser::Ewinstore
-    #    @database.import(opts['inputfile'])
-    #    @database.cull(100)
-    #    @database.export(opts['outputfile'])
-
-    #    Ewinparser::ewinstore.import(opts['inputfile'])
-
-    #    @output = Ewinparser::Stix_parser.parse('C:\Users\tpegg\Desktop\privilege\IB-14-20059.stix.xml')
-    #    @output = Ewinparser::Spreadsheet_parser.parse('C:\Users\tpegg\Desktop\privilege\ANUNAK_02-20-2015.xlsx')
-    #    puts @clean_output
-    @files = Dir.entries(opts['directory']).select {|f| !File.directory? f}
-
-    Dir.chdir opts['directory']
-
-    @stix_files = @files.grep(/(.*)[Ss][Tt][Ii][Xx]\.[Xx][Mx][Ll]$/)
-    @xlxs_files = @files.grep(/(.*)[Xx][Ll][Ss][Xx]$/)
-    @xlx_files = @files.grep(/(.*)[Xx][Ll][Ss]$/)
-    @csv_files = @files.grep(/(.*)[Cc][Ss][Vv]$/)
-
+    @database.import(opts['inputfile']) if !opts['inputfile'].nil? 
     @parsed_files = Array.new
+    @files = Array.new
+    
+    
+    if !opts['directory'].nil?
+      @files = Dir.entries(opts['directory']).select {|f| !File.directory? f}
 
-    # TODO clean input and validate emails
-    @files.each do |file|
-      
-      
-      if file =~ /(.*)[Ss][Tt][Ii][Xx]\.[Xx][Mm][Ll]$/
-        @output_hash = Hash.new()
-        @output_hash = Ewinparser::Stix_parser.parse(file)
+      Dir.chdir opts['directory']
+
+      @stix_files = @files.grep(/(.*)[Ss][Tt][Ii][Xx]\.[Xx][Mx][Ll]$/)
+      @xlxs_files = @files.grep(/(.*)[Xx][Ll][Ss][Xx]$/)
+      @xlx_files = @files.grep(/(.*)[Xx][Ll][Ss]$/)
+      @csv_files = @files.grep(/(.*)[Cc][Ss][Vv]$/)
 
 
-        if (!@output_hash.nil?)
-          @clean_hash = Ewinparser::clean_hash(@output_hash)
-          @database.add_hash( @clean_hash )
-          @parsed_files.push([file, @clean_hash.length])
+      # TODO clean input and validate emails
+      if !@files.nil?
+        @files.each do |file|
+
+          if file =~ /(.*)[Ss][Tt][Ii][Xx]\.[Xx][Mm][Ll]$/
+            @output_hash = Hash.new()
+            @output_hash = Ewinparser::Stix_parser.parse(file)
+
+            if (!@output_hash.nil?)
+              @clean_hash = Ewinparser::clean_hash(@output_hash)
+              @database.add_hash( @clean_hash )
+              @parsed_files.push([file, @clean_hash.length])
+            end
+          end
+
+          if file =~ /(.*)[Xx][Ll][Ss][Xx]$/
+
+            @output_hash = Hash.new()
+            @output_hash = Ewinparser::Spreadsheet_parser.parse(file)
+
+            if (!@output_hash.nil?)
+              @clean_hash = Ewinparser::clean_hash(@output_hash)
+              @database.add_hash( @clean_hash )
+              @parsed_files.push([file, @clean_hash.length])
+            end
+          end
+
+          # TODO Handle CSV files.
+          #      if file =~ /(.*)[Cc][Ss][Vv]$/
+          #        @database.add_hash( Ewinparser::Spreadsheet_parser.parse(file) )
+          #        @parsed_files.push(file)
+          #      end
+
         end
       end
 
-      
-      if file =~ /(.*)[Xx][Ll][Ss][Xx]$/
-        
-        @output_hash = Hash.new()
-        @output_hash = Ewinparser::Spreadsheet_parser.parse(file)
-          
-        if (!@output_hash.nil?)
-          @clean_hash = Ewinparser::clean_hash(@output_hash)
-          @database.add_hash( @clean_hash )
-          @parsed_files.push([file, @clean_hash.length])
-        end
-      end
-
-      # TODO Handle CSV files.
-#      if file =~ /(.*)[Cc][Ss][Vv]$/
-#        @database.add_hash( Ewinparser::Spreadsheet_parser.parse(file) )
-#        @parsed_files.push(file)
-#      end
-
-      # TODO Need to be able to update the database with PDF findings.
-      
     end
 
-    Ewinparser::Ewinstore.export(opts['outputfile'])
-    
+    if !opts['manualfile'].nil?
+      @manual_hash = Hash.new
+      File.open(opts['manualfile']).each do |line|
+        @entry, @entryfile = line.match(/(^.*),(.*)/).captures
+        if @entry =~ /\b(?:\d{1,3}\.){3}\d{1,3}\b/
+          @manual_type = 'ipobj'
+        end
+
+        if (@entry =~ /[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}/ and !@entry.include?('@'))
+          @manual_type = 'domainnameobj'
+        end
+
+        if @entry.include?("@")
+          @manual_type = 'emailmessageobj'
+        end
+
+        if @entry
+          @manual_hash[@entry] = [@manual_type, 'manual', @entryfile, Time.now.to_s]
+        end
+      end
+      @database.add_hash( @manual_hash )
+      @files.push("Manual File Entry")
+      @parsed_files.push(["Manual File Entry", @manual_hash.length])
+    end
+
+
+    # TODO test output when just adding a manual file.
     puts "%s results:" % [Ewinparser::CliParser.command_name]
-    puts "\t Files provided: %4s" % [@files.length ]
-    puts "\t Files parsed:   %4s" % [@parsed_files.length]
-    puts "\t Entries found:  %4s" % [@database::ewin_store_length]
+    puts "\t Files provided:        %4s" % [@files.length ]
+    puts "\t Files parsed:          %4s" % [@parsed_files.length]
+    @all_entries = 0
+    @parsed_files.each do | file |
+      @all_entries += file[1]
+    end
+    puts "\t All Enties found:      %4s" % [@all_entries]
+    puts "\t Unique Entries found:  %4s" % [@database::ewin_store_length]
     puts "Files Parsed: "
     @parsed_files.each do | file |
       puts "\t %-35s Entries: %4s" % [file[0],file[1]]
     end
-    
-    puts 
+
+    puts
     puts
 
+    @database.cull(365)
+    @database.export(opts['outputfile'])
+
     Ewinparser::Printer.print_ticket(@files)
-    
+
   rescue Errno::ENOENT => e
     $stderr.puts e.message
   end
