21715939325394f31664f6126c8e5ce80b02a389
Tim <tim.pegg@gmail.com>
2015-04-17 06:56:28 -0500

    Refactored code to use local variables.

--- stix parser/lib/ewinparser/scrubber.rb
+++ stix parser/lib/ewinparser/scrubber.rb
@@ -1,36 +1,34 @@
 module Ewinparser
-  def self.clean_input(input)
-    @input = input
+  def self.clean_input(value)
+    input = value
 
-    @input = @input.gsub(/\[@\]/,"@")
-    @input = @input.gsub(/\[at\]/,"@")
-    @input = @input.gsub(/\[dot\]/,".")
-    @input = @input.gsub(/\[\.\]/,".")
-    @input = @input.gsub(/[hH][Tt]{2}[Pp]:\/\//,"")
-    @input = @input.strip
-    @input = @input.gsub(/:[0-9]+.*/,"")
-    @input = @input.gsub(/\/.*/,"")
-    @input = @input.gsub(/^.*</,"")
-    @input = @input.gsub(/>.*$/,"")
+    input.gsub!(/\[@\]/,"@")
+    input.gsub!(/\[at\]/,"@")
+    input.gsub!(/\[dot\]/,".")
+    input.gsub!(/\[\.\]/,".")
+    input.gsub!(/[hH][Tt]{2}[Pp]:\/\//,"")
+    input.strip
+    input.gsub!(/:[0-9]+.*/,"")
+    input.gsub!(/\/.*/,"")
+    input.gsub!(/^.*</,"")
+    input.gsub!(/>.*$/,"")
     
     # check for private IP addresses
-    if @input =~ /(^127\.0\.0\.1)|(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.)/
-      @input == nil
+    if input =~ /(^127\.0\.0\.1)|(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.)/
+      input == nil
     end
-    @input
+    input
   end
 
-  def self.clean_hash(hash)
-    @inhash = hash
+  def self.clean_hash(value)
+    inhash = value
+    clean_output = Hash.new
 
-    @clean_output = Hash.new
-
-    @inhash.each do |k,v|
-      @key = Ewinparser.clean_input(k)
-      @clean_output[@key] = v unless  @key.nil? 
+    inhash.each do |k,v|
+      clean_output[Ewinparser.clean_input(k)] = v unless  Ewinparser.clean_input(k).nil? 
     end
 
-    @clean_output
+    clean_output
 
   end
 
